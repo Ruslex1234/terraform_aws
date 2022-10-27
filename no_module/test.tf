18b8bb3c1a343efe3a93cc553f3b24e15164146d
Rodney Rosario <lilrod1234@msn.com>
2022-10-27 04:16:28 +0000

    i tried to create everything without modules but there is way too many dependencies this was my idea to show case what its like to have a module and what its like to not have a module but instead I will hop in and do the modules

--- no_module/test.tf
+++ no_module/test.tf
@@ -23,5 +23,25 @@ resource "aws_subnet" "publicsub2" {
   vpc_id = aws_vpc.rodsvpc.id
   cidr_block = "10.42.4.0/24"
 }
+# Create NAT gateway one
+resource "aws_nat_gateway" "natgatewayone" {
+  subnet_id     = aws_subnet.privatesub1.id
+}
+# Create NAT gateway two
+resource "aws_nat_gateway" "natgatewaytwo" {
+  subnet_id     = aws_subnet.privatesub2.id
+}
+# Create Internet gateway one
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.rodsvpc.id
+}
+# Create route table private one
+resource "aws_route_table" "privateroutetableone" {
+  vpc_id = aws_vpc.rodsvpc.id
+  route {
+    cidr_block = "10.42.1.0/24"
+    gateway_id = "natgatewayone"
+  }
 
+}
 
